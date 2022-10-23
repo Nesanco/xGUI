26b1c6976e1b4be4c03c357acc5e3f1aa426366f
Nesanco <80917510+Nesanco@users.noreply.github.com>
2022-10-22 23:50:54 -0400

    Update ItemBuilder.java

--- src/main/java/nesancodev/com/xgui/util/ItemBuilder.java
+++ src/main/java/nesancodev/com/xgui/util/ItemBuilder.java
@@ -6,27 +6,29 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
 
 public class ItemBuilder {
 
     public static ItemStack buildItem(Material material, String name,
-                               String lore, Integer modeldata) {
+                               List<String> lore, Integer modeldata) {
         ItemStack it1 = new ItemStack(material);
         ItemMeta it1meta = it1.getItemMeta();
         String itm1 = t(name);
         it1meta.setCustomModelData(modeldata);
-        it1meta.setLore(Collections.singletonList(t(lore)));
+        it1meta.setLore(t(lore));
         it1meta.setDisplayName(itm1);
         it1.setItemMeta(it1meta);
         return it1;
     }
 
     public static ItemStack buildItem(Material material, String name,
-                               String lore) {
+                                      List<String> lore) {
         ItemStack it1 = new ItemStack(material);
         ItemMeta it1meta = it1.getItemMeta();
         String itm1 = t(name);
-        it1meta.setLore(Collections.singletonList(t(lore)));
+        it1meta.setLore(t(lore));
         it1meta.setDisplayName(itm1);
         it1.setItemMeta(it1meta);
         return it1;
@@ -47,5 +49,13 @@ public class ItemBuilder {
 
     private static String t(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    private static List<String> t(List<String> message) {
+        List<String> stringList = new Stack<>();
+        for (String str : message) {
+            stringList.add(t(str));
+        }
+        return stringList;
     }
 }
